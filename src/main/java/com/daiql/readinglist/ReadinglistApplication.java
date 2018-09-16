5d6ae616ccdfaccbe996efe02cfdc06c791c349e
daiql <371934907@qq.com>
2018-09-16 12:15:00 +0800

    First Commit! SpringBoot In Action Demo

---
+++ src/main/java/com/daiql/readinglist/ReadinglistApplication.java
@@ -0,0 +1,12 @@
+package com.daiql.readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //开启组件扫描和自动配置
+public class ReadinglistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadinglistApplication.class, args); //负责启动引导应用程序
+    }
+}
